5f78b46da5571c9be160a5bc60f4ab931642adf4
NatachaVergara <ntchvergara@gmail.com>
2021-05-21 12:51:58 -0300

    Por alguna razon antes no me tomó todas las funciones

--- js/scriptRest.js
+++ js/scriptRest.js
@@ -1,11 +1,30 @@
 console.log("linqueado")
 //Funcion para toogle para mostrar masInformacion en la seccion Restaurantes (cartas)
-let carta1 = document.querySelector(".info")
+let carta1 = document.querySelectorAll(".info")[0]
 
 carta1.addEventListener("click", () =>{
 
-       mostrarCarta1 = document.querySelector(".primero1").classList.toggle("primero")
+       mostrarCarta1 = document.querySelector(".primero1").classList.toggle("primero")  
+
+} )
+
+
+let carta2 = document.querySelectorAll(".info")[1]
+
+carta2.addEventListener("click", () =>{
+
+    mostrarCarta2 = document.querySelector(".segundo").classList.toggle("medio")  
+
+} )
+
+
+let carta3 = document.querySelectorAll(".info")[2]
+
+carta3.addEventListener("click", () =>{
+
+    mostrarCarta3 = document.querySelector(".tercero").classList.toggle("ultimo");  
 
   
+
 
 } )
